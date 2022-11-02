f06174fb34aa0254399fd8b18c628adf06bf3df5
Noah Shinn <noahshinn024@gmail.com>
2022-11-02 02:54:16 -0400

    2d rand lst

--- k_state_gen/bin/main.ml
+++ k_state_gen/bin/main.ml
@@ -11,6 +11,16 @@ module Computer = struct
     let max_ = List.fold_left max (List.hd t1) (List.tl t1) in
     let min_ = List.fold_left min (List.hd t1) (List.tl t1) in
     List.map (fun a -> div a max_) (List.map (fun b -> sub b min_) t1)
+  let rand_lst dim0 dim1 =
+    let rec rand_int_lst acc count =
+      match count with
+      | 0 -> acc
+      | _ -> rand_int_lst (List.cons Random.int acc) (count - 1) in
+    let rec rand_lst_lst acc count =
+      match count with
+      | 0 -> acc
+      | _ -> rand_lst_lst (List.cons (rand_int_lst [] dim1) acc) (count - 1) in
+    rand_lst_lst [] dim0
   end;;
 
 (*loaders.ml*)
@@ -18,7 +28,7 @@ module Computer = struct
   (*let load_csv path = Csv.load*)
   (*end;;*)
 
-let () =
-  List.iter (printf "%f ") (Computer.normalize (List.map float_of_int [ 0; 1; 2 ]))
+(*let () =*)
+  (*List.iter (printf "%f ") (Computer.normalize (List.map float_of_int [ 0; 1; 2 ]))*)
 
 (** k-means classification + model-based RL agent *)
