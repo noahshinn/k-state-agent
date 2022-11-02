8ba629b8ca1efdfbbb569107d201b77afedd78c5
Noah Shinn <noahshinn024@gmail.com>
2022-11-02 03:31:29 -0400

    eucl dist

--- k_state_gen/bin/main.ml
+++ k_state_gen/bin/main.ml
@@ -7,7 +7,6 @@ open Float
 (*computer.ml*)
 module Computer = struct
   let normalize t1 =
-    (*let div n k = (n / k) + (n mod k) in*)
     let max_ = List.fold_left max (List.hd t1) (List.tl t1) in
     let min_ = List.fold_left min (List.hd t1) (List.tl t1) in
     List.map (fun a -> div a max_) (List.map (fun b -> sub b min_) t1)
@@ -21,6 +20,17 @@ module Computer = struct
       | 0 -> acc
       | _ -> rand_lst_lst (List.cons (rand_int_lst [] dim1) acc) (count - 1) in
     rand_lst_lst [] dim0
+  let state_eucl_dist s1 s2 =
+    let eucl_dist t1 t2 =
+      sqrt
+      (List.fold_left
+        add
+        0.0
+        (List.map2
+          (fun a b -> pow (sub a b) 2.)
+          t1
+          t2)) in
+    List.map2 (fun a b -> eucl_dist a b) s1 s2
   end;;
 
 (*loaders.ml*)
@@ -28,7 +38,7 @@ module Computer = struct
   (*let load_csv path = Csv.load*)
   (*end;;*)
 
-(*let () =*)
-  (*List.iter (printf "%f ") (Computer.normalize (List.map float_of_int [ 0; 1; 2 ]))*)
+let () =
+  List.iter (printf "%f ") (Computer.state_eucl_dist [[0.; 0.; 0.]; [0.; 0.; 0.]; [0.; 0.; 0.]] [[1.; 1.; 1.;]; [1.; 1.; 1.;]; [1.; 1.; 1.;]])
 
 (** k-means classification + model-based RL agent *)
